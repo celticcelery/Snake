99c3b5bd841f0e07113176203eccd01eac563052
celticcelery <celticcelery@gmail.com>
2018-08-21 12:26:41 -0400

    Added closeBtn and replayBtn classes

---
+++ core/src/com/mygdx/game/ReplayBtn.java
@@ -0,0 +1,21 @@
+package com.mygdx.game;
+
+
+import com.badlogic.gdx.graphics.Texture;
+
+public class ReplayBtn {
+	private int x, y;
+	Texture texture;
+	public ReplayBtn(int x, int y) {
+		this.x = x;
+		this.y = y;
+		texture = Storage.assetManager.get("replay.png",Texture.class);
+	}
+	public void draw(){
+		Storage.batch.draw(texture, x, y);
+	}
+	public boolean isClicked(float touchX, float touchY){
+
+		return touchX >= x && touchX < x + texture.getWidth() && touchY >= y && touchY < y + texture.getHeight() ;
+	}
+}
